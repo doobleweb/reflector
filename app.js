07335e5c26d122a1350bb4a4e11ca4445d27d768
Stacy Semo <stacysemo1@gmail.com>
2019-04-09 21:23:20 +0300

    adding function for build data
    remove duplicated code
    removed unnecessary code from body
    added function for inner api call.. post tpo user.data with team id and username and get data on the user

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');   // need inorder to parse body in line
 var he = require('./he');                  // supllies the function that will reflect the text
 require('dotenv').config();                // required for .env file
 
-var http = require('http');                // Needed modules for parsing HTML files that are in the
-var fs = require('fs');                    // public folder
+var http = require('http');                // Needed modules for parsing HTML files that are in the public folder
+var fs = require('fs');                    //
 
 const storage = require('node-persist');   //storage module for saving the token outside the app
 storage.init();
@@ -85,7 +85,7 @@ app.get('/success', function(req, res){
 app.post('/slackReflector',function(req,res){
     if(req.body.token !== process.env.SLACK_VERIFICATION_TOKEN) {
       // the request is NOT coming from Slack!
-      res.sendStatus(401);
+        res.sendStatus(401);
       return;
     } else {
       getReply(req.body)
@@ -95,30 +95,71 @@ app.post('/slackReflector',function(req,res){
     }
 });
 
+// Reply to channel according to given word in the body
+// if the user sent /reflect with following text the app will take that text and reflect it
+// else if the user sent just /reflect without text the app will take the last message text and reflect it
+const getReply = function (body){
+  return new Promise(function (resolve, reject) {
+    let data = {};
+    getToken(body.team_id)
+    .then((tokenRes) => {
+      if(body.text)
+      {
+        return resolve(buildData('in_channel', 'Reflecting ' + body.text, he.decideLang(body.text)));
+      }
+      else
+      { // no text entered
+        getLastWord(tokenRes, body.channel_id, process.env.SLACK_API_CONVERSATION_HISTORY_URL)
+        .then((lastWordRes) => {
+          if (lastWordRes['subtype'] !== "bot_message"){  //check if last message was from reflector app
+            data = buildData('in_channel', 'Reflecting ' + lastWordRes['text'], he.decideLang(lastWordRes['text']));
+          } else {  //if last message was from the app return error
+            data = buildData('in_channel', 'Reflector Error', 'Reflector cannot be used twice in a row');
+           }
+           return resolve(data);
+         }).catch(console.error);
+       }
+     }).catch(console.error);
+    });
+  }
+
+//build data that is sent back to slack channel
+const buildData = function (type,text,attachText) {
+  data = {
+    response_type: type, // private message
+    text: text,
+    attachments:[{
+      text: attachText
+    }
+  ]};
+  return data;
+  }
+
 //get token function from the storage instance
 const getToken = function(team){
-  return  new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     let oauthToken = storage.getItem(team);
     return resolve(oauthToken);
   });
 }
 
 //get user full real name for Reflector answer
-const getUserFullname = function (token, user, uri) {
+const getUserFullData = function (token, username, uri) {
   return new Promise(function(resolve, reject) {
-    var getName = {
+    var getUserData = {
            uri : uri,
            method : 'GET',
            qs : {
-             'token' : token,
-             'user' :  user
+             'token' : token
            }
          }
-    request(getName, function (error, response, body) {
+    request(getUserData, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-         return resolve(JSON.parse(body).user.real_name);
+        for (var i=0;i<JSON.parse(body).members.length;i++)
+          if (JSON.parse(body).members[i].name == username)
+            return resolve(JSON.parse(body).members[i]);
        } else {
-         return resolve('The user');
+         return resolve('Unable to find the user with the user name ' + username);
        }
      });
   });
@@ -145,68 +186,8 @@ const getLastWord = function(token,channel_id, uri) {
   });
 }
 
-// Reply to channel according to given word in the body
-// if the user sent /reflect with following text the app will take that text and reflect it
-// else if the user sent just /reflect without text the app will take the last message text and reflect it
-const getReply = function (body){
-  return new Promise(function (resolve, reject) {
-    let data = {};
-    if(body.text) {
-      getToken(body.team_id)
-      .then((tokenRes) => {
-          getUserFullname(tokenRes, body.user_id, process.env.SLACK_API_USERS_INFO_URL)
-            .then((userRes) => {
-              var resObj =he.decideLang(body.text);  //reflect
 
-              data = {
-                response_type: 'in_channel', // public to the channle
-                text: 'Reflecting ' + body.text,
-                attachments:[{
-                  text: resObj
-                }]
-              };
-              return resolve(data);
-            })
-            .catch(console.error);
-          })
-        .catch(console.error);
-
-    } else { // no query entered
-      getToken(body.team_id)
-        .then((tokenRes) => {
-          getUserFullname(tokenRes, body.user_id, process.env.SLACK_API_USERS_INFO_URL)
-            .then((userRes) => {
-              getLastWord(tokenRes, body.channel_id, process.env.SLACK_API_CONVERSATION_HISTORY_URL)
-               .then((lastWordRes) => {
-                 if (lastWordRes['subtype'] !== "bot_message"){
-                   var resObj =he.decideLang(lastWordRes['text']);  //reflect
-                   data = {
-                    response_type: 'in_channel', // private message
-                    text: 'Reflecting ' + lastWordRes['text'],
-                    attachments:[{
-                      text: resObj
-                    }
-                  ]};
-                } else {
-                  data = {
-                   response_type: 'in_channel', // private message
-                   text: 'Reflector Error',
-                   attachments:[{
-                     text: 'Reflector cannot be used twice in a row'
-                   }
-                 ]};
-                }
-                return resolve(data);
-               })
-               .catch(console.error);
-             })
-             .catch(console.error);
-           })
-           .catch(console.error);
-  }
-  });
-}
-
+////////////////////////////////////////////////////////////////////////////////////////////////
 //api call from postman try
 app.get('/team/:id', function (req, res) {
   getToken(req.params.id)
@@ -221,6 +202,33 @@ app.get('/team/:id', function (req, res) {
 
 //post call to reflector with team id as a parameter and get in return the team token
 app.post('/team/token', function (req,res) {
-  var id = req.body.id;
+  var id = req.body.team_id;
   res.redirect('/team/' + id);
+});
+
+//api call from postman try 2//
+app.get('/user/:userID/:teamID/:username/:realName', function (req, res) {
+  getToken(req.params.teamID)
+  .then((tokenRes) => {
+      res.send({
+        'Real name': req.params.realName,
+        'username' : req.params.username,
+        'user ID'  : req.params.userID,
+        'team ID': req.params.teamID,
+        'Access token' : tokenRes
+      });
+  })
+  .catch(console.error);
+});
+
+//post call to reflector with team id and username and get all user Data
+app.post('/user.Data', function (req,res) {
+  var id;
+  getToken(req.body.team_id)
+  .then((tokenRes) => {
+    getUserFullData(tokenRes, req.body.username, process.env.SLACK_API_USERS_LIST_URL)
+    .then((userRes) => {
+      res.redirect('/user/' +  userRes.id +"/"+ userRes.team_id+"/"+ userRes.name+"/"+ userRes.real_name);
+    }).catch(console.error);
+  }).catch(console.error);
 });
